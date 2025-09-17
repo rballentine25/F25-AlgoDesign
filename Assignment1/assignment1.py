6a48869d2c6959efbdb39d0dfcd65552181fa401
rballentine25 <ballentine.7@wright.edu>
2025-09-17 13:27:37 -0400

    finished pref lists and ranking methods

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -5,12 +5,93 @@ Assignment 1
 import os
 dirpath = os.path.dirname(os.path.abspath(__file__))
 
-manPref = []
-womanPref = []
-ranking = []
-current = None
-next = None
-freeMen = None
+class StableMatchSetup:
+    def __init__(self, filename):
+        # establish all global variables
+        self.n = None
+        self.manPref = []
+        self.womanPref = []
+        self.ranking = []
+
+        self.nameMap = {}
+        self.allNames = []
+        self.filename = filename
+
+        # create name map
+        self.createNameMap()
+        self.createPrefLists()
+
+        if self.n is not None:
+            self.current = [None]*self.n
+            self.next = [0]*self.n
+            self.freeMen = [i for i in range(self.n)]
+
+
+    def createNameMap(self):
+        file = open(self.filename, "r")
+
+        self.n = int(file.readline().strip()) # just read n
+
+        for line in file:
+            names = line.split()
+            self.allNames.append(names)
+
+        # assuming input file has names in alphabetical order
+        row = 0
+        # assign numbers to the men: rows 0:n
+        for i in range(self.n): 
+            self.nameMap[self.allNames[row][0]] = i 
+            row += 1
+
+        # assign numbers to the women: rows n+1:2n
+        for j in range(self.n):
+            self.nameMap[self.allNames[row][0]] = j
+            row += 1
+
+
+    def createPrefLists(self):
+        # create the men's preference lists
+        for m in range(self.n):
+            self.manPref.append([])
+            for w in range(1, self.n+1):
+                # in the current row (man), match the next woman from the input file to her
+                # id in the name map and append that id to the man pref matrix
+                self.manPref[m].append(self.nameMap[self.allNames[m][w]])
+
+        # create the women's preference lists
+        # since allNames has to be indexed between n:2n-1 (0 based) to get correct row, 
+        # need another var to keep track of current row in womanPref
+        prefRow = 0
+        for w in range(self.n, 2*self.n): # second half of the input matrix
+            self.womanPref.append([])
+            for m in range(1, self.n+1):
+                # for each women, match each man in her list to his id and add to pref list
+                self.womanPref[prefRow].append(self.nameMap[self.allNames[w][m]])
+            prefRow += 1
+
+        print("manPref list: ", self.manPref)
+        print("womanPref list: ", self.womanPref)
+
+        # once the preference lists have been created, call createRanking to make the women's ranking list
+        self.createRanking()
+        
+
+    def createRanking(self):
+        # create an nxn empty matrix for womens rankings
+        self.ranking = [[0]*self.n for x in range(self.n)]
+
+        # fill in the ranking linearly with a single pass through women's pref list (n^2)
+        # for each man in the women's pref list, remember his id and use that to index into
+        # the ranking list. insert the current val i for his pref ranking in the ranking list
+        for w in range(self.n):
+            for i in range(self.n):
+                rank = self.womanPref[w][i]
+                self.ranking[w][rank] = i
+        
+        print("ranking: ", self.ranking)
+        
+
+
 
 def main():
     # validating input file name
@@ -29,26 +110,15 @@ def main():
         else: 
             print("filename invalid")
 
-    if not valid:
-        return
+    sm = StableMatchSetup(filename)
 
-    createNameMap(filename)
-    
+    print(sm.allNames, "\n", sm.nameMap)
+    print("\n")
 
-    
-def createNameMap(filename):
-    allnames = []
-    n = None
-    file = open(filename, "r")
+    #createPrefLists()
+    #print(globals.manPref)
 
-    n = file.readline().strip() # just read n
 
-    for line in file:
-        names = line.split()
-        allnames.append(names)
-    
-    print(n)
-    print(allnames)
 
 
 main()
--- Assignment1/random.py
+++ Assignment1/random.py
@@ -13,6 +13,39 @@ print(freeMen)
 print(os.getcwd())
 print(os.path.abspath(__file__))
 
+class NewClass:
+    def __init__(self, number):
+        self.n = number
+    
+
+
+#n = None
+def main():
+    nc = NewClass(5)
+    print(nc.n)
+
+    num = 3
+    empty = []
+    for i in range(num):
+        empty.append([0]*num)
+
+    empty2 = [[0]*num for i in range(num)]
+
+    print(empty)
+    print(empty2)
+
+    list1 = [0, 3, 5, 8, 2, 11]
+    list2 = [x for x in list1 if x>3]
+    print(list2)
+
+def method1(n):
+    n = 4
+
+def method2(n):
+    print(n)
+    n = 6
+
+main()
 """
 can also do:
 inputfile = sys.argv[1] # within the script
